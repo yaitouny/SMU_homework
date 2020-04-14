16df54e218776255a5028399381f2de119db1ab5
Yasmine Aitouny <aitouny680@gmail.com>
2020-04-13 22:54:00 -0500

    cleaned code

--- Python-Challenge/PyBank/main.py
+++ Python-Challenge/PyBank/main.py
@@ -10,18 +10,23 @@ changeList = []
 maxIncrease = 0
 minDecrease = 0
 
-
+#open and read from csv file
 with open(budget_data) as csvfile:
 	csv_reader = csv.reader(csvfile, delimiter = ",")
 
 	csv_header = next(csvfile)
 	#print(f"Header: {csv_header}")
 
+	#itirate through each row
 	for row in csv_reader:
+		#find the number of month in the file
 		totalMonths += 1
+
+		#find the net total 
 		netTotal += int(row[1])
 		newList.append(row[1])
 
+		#find maximum and the minimum of the profits/losses
 		if int(row[1]) > maxIncrease:
 			maxIncrease = int(row[1])
 			bestMonth = row[0]
@@ -30,12 +35,13 @@ with open(budget_data) as csvfile:
 			minDecrease = int(row[1])
 			worstMonth = row[0]
 			
-
+	#find the change then calculate its average 
 	for i in range(len(newList) - 1):
 		changeList.append(int(newList[i+1]) - int(newList[i]))
 	
 	averageChange = round(sum(changeList) / len(changeList), 2)
 	
+	#print final results 
 	print("Financial Analysis")
 	print("------------------------------------")
 	print(f"Total Months: {totalMonths}")
@@ -46,7 +52,7 @@ with open(budget_data) as csvfile:
 	print("\n")
 
 
-
+# write output to a csvfile 
 output = r"Python-Challenge/PyBank/Analysis Summary.csv"
 
 with open(output, 'w') as datafile:
