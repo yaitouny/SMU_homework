7a44c7310924fa916b48bcfaf74102c871244883
Yasmine Aitouny <aitouny680@gmail.com>
2020-05-03 18:33:55 -0500

    add linear plots

--- 06-Python-APIs/python-api-challenge/starter_code/api_keys.py
+++ 06-Python-APIs/python-api-challenge/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = ""
+weather_api_key = "705e35f1e9dc50faaf8b89051e7f4d66"
 
 # Google API Key
-g_key = ""
+g_key = "AIzaSyCetKV6CFKyvnY9bAQiQH_IVwu63HeB9GU"
