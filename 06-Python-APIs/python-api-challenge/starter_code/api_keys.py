1fe959e43c640386b30856a794456932ad597a5a
Yasmine Aitouny <aitouny680@gmail.com>
2020-05-04 15:05:47 -0500

    removed api keys for security

--- 06-Python-APIs/python-api-challenge/starter_code/api_keys.py
+++ 06-Python-APIs/python-api-challenge/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "705e35f1e9dc50faaf8b89051e7f4d66"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyCetKV6CFKyvnY9bAQiQH_IVwu63HeB9GU"
+g_key = ""
